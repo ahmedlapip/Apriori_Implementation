3483c5b26d8b53f941f57bda35fe0512df145ad8
YousefAdel777 <www.yoyoadel@gmail.com>
2025-11-13 01:28:33 +0200

    feat: add lift calculation and rules generation

--- Apriori.py
+++ Apriori.py
@@ -2,28 +2,50 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from sortedcontainers import SortedSet
 
-
 class Itemset:
-    def __init__(self, itemset: SortedSet, sup: float):
-        self.items = itemset
+    def __init__(self, items: SortedSet, sup: float, sup_count: int):
+        self.items = items
         self.sup = sup
+        self.sup_count = sup_count
     
     def __str__(self):
-        return str(self.items)
+        return f"{{ {", ".join(self.items) } }}"
     
     def __repr__(self):
-        return repr(self.items)
+        return f"{{ {", ".join(self.items) } }}"
+
+class Rule:
+    def __init__(self, left: SortedSet, right: SortedSet, itemset: Itemset, conf: float, lift: float):
+        self.left = left
+        self.right = right
+        self.conf = conf
+        self.itemset = itemset
+        self.lift = lift
+
+    def __str__(self):
+        return f"{{ {", ".join(self.left) } }} -> {{ {", ".join(self.right)} }}"
+
+    def __repr__(self):
+        return f"{{ {", ".join(self.left) } }} -> {{ {", ".join(self.right)} }}"
+    
+    def is_strong(self, min_conf):
+        return self.conf >= min_conf
 
 class Apriori :
 
-    def __init__(self, file_path, min_sup):
-        self.data = self.Prepare_data(file_path, min_sup)
+    def __init__(self, file_path, min_sup, min_conf):
+        self.data = self.Prepare_data(file_path)
         self.min_sup = min_sup
+        self.min_conf = min_conf
+        self.rules = []
+        self.freq_sets = {}
+        self.itemsets = {}
 
     def run(self):
         self.freq_sets = {}
         self.itemsets = {}
         l = SortedSet()
+        self.rules = [[]]
         for tran in self.data:
             for item in tran:
                 if item in l:
@@ -31,30 +53,47 @@ class Apriori :
                 itemset = SortedSet([item])
                 sup = self.Sup(itemset)
                 itemsets = self.itemsets.get("C1", [])
-                itemsets.append(Itemset(itemset, sup))
+                itemsets.append(Itemset(itemset, sup / len(self.data), sup))
                 self.itemsets["C1"] = itemsets
                 if sup >= self.min_sup:
                     itemsets = self.freq_sets.get("L1", [])
-                    itemsets.append(Itemset(itemset, sup))
+                    itemsets.append(Itemset(itemset, sup / len(self.data), sup))
                     self.freq_sets["L1"] = itemsets
                     l.add(item)
         l = [SortedSet(x) for x in l]
         self.generate_itemsets(self.join(l), 2)
+
+    def generate_rules(self, itemset: Itemset, left: SortedSet, right: SortedSet, level: int):
+        if level == len(itemset.items):
+            if left and right:
+                conf = self.Conf(self.data, left, right)
+                sup = self.Sup(right) / len(self.data)
+                self.rules[-1].append(Rule(left, right, itemset, conf, conf / sup))
+            return
+        self.generate_rules(itemset, left, right, level + 1)
+        new_left = SortedSet(left)
+        new_right = SortedSet(right)
+        new_left.add(itemset.items[level])
+        if itemset.items[level] in new_right:
+            new_right.remove(itemset.items[level])
+        self.generate_rules(itemset, new_left, new_right, level + 1)
     
     def generate_itemsets(self, data, level):
         if not data:
             return
+        self.rules.append([])
         l = []
         for itemset in data:
             sup = self.Sup(itemset)
             itemsets = self.itemsets.get(f"C{level}", [])
-            itemsets.append(Itemset(itemset, sup))
+            itemsets.append(Itemset(itemset, sup / len(self.data), sup))
             self.itemsets[f"C{level}"] = itemsets
             if sup >= self.min_sup:
                 itemsets = self.freq_sets.get(f"L{level}", [])
-                itemsets.append(Itemset(itemset, sup))
+                itemsets.append(Itemset(itemset, sup / len(self.data), sup))
                 self.freq_sets[f"L{level}"] = itemsets
                 l.append(itemset)
+                self.generate_rules(Itemset(itemset, sup / len(self.data), sup), SortedSet(), SortedSet(itemset), 0)
         self.generate_itemsets(self.join(l), level + 1)
 
     def join(self, itemsets: list[SortedSet]):
@@ -65,13 +104,13 @@ class Apriori :
                     joined.append(itemsets[i].union(itemsets[j]))
         return joined
 
-    def Prepare_data(self, Data_path,Min_Sup): ##the Data file [T-ID , Item]
+    def Prepare_data(self, Data_path): ##the Data file [T-ID , Item]
         if str(Data_path).endswith("xlsx"):
             df=pd.read_excel(Data_path)
         elif str(Data_path).endswith("csv"):
             df =pd.read_csv(Data_path)
         else :
-            print("Enter Vaild Path [CSV or XLSX Format]")    
+            raise Exception("Enter Vaild Path [CSV or XLSX Format]")    
         x=list(df['items'])
         l=[]
         for i in x:
@@ -82,15 +121,15 @@ class Apriori :
         for tran in self.data:
             if item .issubset(tran):
                 cnt+=1
-        return cnt        
-    def Conf(self, L:list,item:str):
+        return cnt      
+
+    def Conf(self, L:list, item1: SortedSet, item2: SortedSet):
         cnt1=0
         cnt2=0
         for tran in L :
-            if item .issubset(tran):
+            if item1.union(item2).issubset(tran):
                 cnt1+=1
-        for tran in L :
-            if item[:-1].issubset(tran):
+            if item1.issubset(tran):
                 cnt2+=1 
         return cnt1/cnt2
     
--- main.py
+++ main.py
@@ -4,10 +4,12 @@ import streamlit as st
 import pandas as pd
 
 def visualize():
+
+    i = 0
     for level, data in ap.freq_sets.items():
         df = pd.DataFrame({
-            "itemset": [str(d.items) for d in data],
-            "sup": [d.sup for d in data]
+            "itemset": [str(d) for d in data],
+            "sup": [d.sup_count for d in data]
         })
 
         fig, ax = plt.subplots(figsize=(8, 4))
@@ -19,9 +21,26 @@ def visualize():
 
         st.pyplot(fig)
 
+        rows = []
+        for rule in ap.rules[i]:
+            rows.append({
+                "Itemset": str(rule.itemset),
+                "Rule": str(rule),
+                "Support Count": rule.itemset.sup_count,
+                "Support": rule.itemset.sup,
+                "Confidence": rule.conf,
+                "Lift": rule.lift,
+                "Strong": rule.is_strong(min_conf),
+                "Dependency": "Independent" if rule.lift == 1 else "Positive" if rule.lift > 1 else "Negative"
+            })
+
+        rules_df = pd.DataFrame(rows)
+        st.dataframe(rules_df)
+        i += 1
+
     for level, data in ap.itemsets.items():
         df = pd.DataFrame({
-            "itemset": [str(d.items) for d in data],
+            "itemset": [str(d) for d in data],
             "sup": [d.sup for d in data]
         })
 
@@ -38,6 +57,7 @@ st.title("Frequent Patterns Mining Using Apriori Algortihm")
 
 uploaded_file = st.file_uploader("Upload CSV file", type=["csv", "xlsx"])
 min_sup = st.slider("Select minimum support", min_value=1, max_value=10, step=1, value=2)
+min_conf = st.slider("Select minimum confidence", min_value=0.0, max_value=1.0, step=0.1, value=0.5)
 
 if uploaded_file is not None:
     if uploaded_file.name.endswith("xlsx"):
@@ -47,7 +67,7 @@ if uploaded_file is not None:
     st.dataframe(df)
 
     if st.button("Run Apriori Algorithm"):
-        ap = Apriori(uploaded_file.name, min_sup)
+        ap = Apriori(uploaded_file.name, min_sup, min_conf)
         ap.run()
         visualize()
         st.balloons()
